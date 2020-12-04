cd30e95d0b2d1323ebc95dd0a66098aa7d99e89b
Chris <cbock1337@gmail.com>
2020-12-04 10:17:44 -0500

    Update dismand.go

--- dismand.go
+++ dismand.go
@@ -63,6 +63,9 @@ func New(client *disgord.Client, cfg *Config) *Dismand {
 // RegisterDefaults registers the default Dismand commands
 //
 // - Ping
+// - Help
+// - Enable
+// - Disable
 func (d *Dismand) RegisterDefaults() *Dismand {
 	commands["ping"] = &cmd{
 		c:           pingPong,
