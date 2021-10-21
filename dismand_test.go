d925ea3c5af886b131062cc9ce4a42aa5bd5b920
Chris Bockover <cbock1337@gmail.com>
2021-10-20 21:19:44 -0400

    Update dismand_test.go

--- dismand_test.go
+++ dismand_test.go
@@ -2,6 +2,8 @@ package dismand
 
 import (
 	"testing"
+
+	"github.com/andersfylling/disgord"
 )
 
 func Test_cmd_Description(t *testing.T) {
@@ -47,5 +49,53 @@ func Test_Disgoman_RegisterDefaults(t *testing.T) {
 	_, ok := commands["ping"]
 	if !ok {
 		t.Error("Commands did not contain the default ping command")
+	}
+}
+
+func TestConfig_messageIsInAllowedChannel(t *testing.T) {
+	d := &Dismand{}
+	ptr := d.RegisterDefaults()
+	if ptr == nil {
+		t.Error("RegisterDefaults returned a nil pointer")
+	}
+
+	c := Config{}
+
+	c.AllowedChannels = []uint64{123, 456}
+
+	if c.messageIsInAllowedChannel(678) {
+		t.Error("Failed to detect unallowed channel")
+	}
+
+	if !c.messageIsInAllowedChannel(123) {
+		t.Error("Failed to detech allowed channel")
+	}
+}
+
+func Test_cmd_MinPermission(t *testing.T) {
+	c := cmd{}
+
+	c.MinPermission(disgord.PermissionAll)
+
+	if c.minPerm != disgord.PermissionAll {
+		t.Error("Failed to set minimum permission")
+	}
+}
+
+func Test_cmd_AllowedChannels(t *testing.T) {
+	c := cmd{}
+
+	c.AllowedChannels([]uint64{123, 456})
+
+	if c.allowedChannels == nil {
+		t.Error("Failed to set allowed channels")
+	}
+
+	if c.allowedChannels[0] != 123 {
+		t.Error("Failed to set allowed channels")
+	}
+
+	if c.allowedChannels[1] != 456 {
+		t.Error("Failed to set allowed channels")
 	}
 }
